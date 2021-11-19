9f423acd9465db27b93d505442fd32aa58a5a509
eche-unicorn <d.anasteysha@gmail.com>
2021-11-19 11:02:42 +0200

    list updated

--- src/js/searchStopWords.js
+++ src/js/searchStopWords.js
@@ -3,21 +3,102 @@ import Mark from 'mark.js';
 import createStopWordsList from './createStopWordsList';
 const { textareaAccess } = refs;
 
+// const stopWords = [
+
+//   { word: 'nursing essay', substitutes: ['essay on Biology'] },
+//   { word: 'essay on medicine ', substitutes: ['essay on Biology / or any other subject'] },
+//   { word: 'Free Plagiarism Report', substitutes: ['Free report on originality'] },
+//   {
+//     word: 'a free Turnitin plagiarism report',
+//     substitutes: ['free originality report', 'uniqueness report'],
+//   },
+
+//   {
+//     word: 'unlimited revisions',
+//     substitutes: [
+//       'direct chat with an expert (or any other feature, except free plagiarism report)',
+//     ],
+//   },
+//
+// ];
+
 const stopWords = [
-  { word: 'academic', substitutes: ['educational', 'teaching', 'learning'] },
-  { word: 'nursing essay', substitutes: ['essay on Biology'] },
-  { word: 'essay on medicine ', substitutes: ['essay on Biology / or any other subject'] },
-  { word: 'Free Plagiarism Report', substitutes: ['Free report on originality'] },
   {
-    word: 'a free Turnitin plagiarism report',
-    substitutes: ['free originality report', 'uniqueness report'],
+    word: 'writer',
+    substitutes: [
+      'expert',
+      'tutor',
+      'advisor',
+      'graduate',
+      'assistant',
+      'co-author',
+      'learning strategist',
+      'paper coach',
+      'subject-matter expert',
+      'teaching staff',
+      'instructor',
+      'nerd',
+      'helper',
+      'specialist',
+      'educator',
+      'geek',
+    ],
+  },
+  {
+    word: 'turnitin report',
+    substitutes: [
+      'similarity checker',
+      'originality checker',
+      'originality report',
+      'web matches report',
+      'authenticity report',
+      'Get your text scaned for similarities / web matches',
+    ],
+  },
+  {
+    word: 'plagiarism-free',
+    substitutes: [
+      'similarity checker',
+      'originality checker',
+      'originality report',
+      'web matches report',
+      'authenticity report',
+      'Get your text scaned for similarities / web matches',
+    ],
+  },
+
+  {
+    word: 'plagiarism report',
+    substitutes: [
+      'similarity checker',
+      'originality checker',
+      'originality report',
+      'web matches report',
+      'authenticity report',
+      'Get your text scaned for similarities / web matches',
+    ],
+  },
+
+  {
+    word: 'confidentiality',
+    substitutes: [
+      'privacy',
+      'secrecy',
+      'data protection',
+      'You always own your data — it’s our responsibility to keep it more secure',
+      '1-on-1 help',
+      '1-on-1 guidance',
+      '1-on-1 coaching',
+      '1:1 support from expert tutor',
+      '1:1 learning strategist',
+    ],
+  },
+  {
+    word: 'student',
+    substitutes: ['customer', 'learner', 'client', 'people', 'undergraduate', 'scholar'],
   },
   { word: 'before the deadline', substitutes: ['earlier than asked'] },
   { word: 'deadline', substitutes: ['completion date'] },
-  {
-    word: 'student',
-    substitutes: ['customers', 'learners', 'clients', 'people'],
-  },
   { word: 'academic field', substitutes: ['subject field'] },
   { word: 'buy assignment ', substitutes: ['outsource assignment'] },
   { word: 'assessment task', substitutes: ['task', 'assignment'] },
@@ -31,7 +112,7 @@ const stopWords = [
   { word: 'strictly against plagiarism', substitutes: ['stands for originality of papers'] },
   { word: 'boost grades', substitutes: ['help you do better with your tasks'] },
   {
-    word: 'Enhance your academic grade',
+    word: 'enhance your academic grade',
     substitutes: ['start to do better at school/college/university'],
   },
   {
@@ -39,22 +120,48 @@ const stopWords = [
     substitutes: ['If you have to write an essay but have no clue on where to start'],
   },
   {
-    word: 'writer',
-    substitutes: ['expert', 'author'],
-  },
-  {
     word: 'to write',
     substitutes: ['to accomplish', 'to create', 'to develop'],
   },
   {
-    word: 'unlimited revisions',
+    word: 'essay writing service',
+    substitutes: ['essay advice service'],
+  },
+  { word: 'academic', substitutes: ['educational', 'teaching', 'learning'] },
+
+  {
+    word: 'customized paper',
     substitutes: [
-      'direct chat with an expert (or any other feature, except free plagiarism report)',
+      "content feedback', 'personalized writing advice",
+      'made-to-order paper',
+      'personalized feedback',
+      'personalized writing tips',
     ],
   },
   {
-    word: 'essay writing service',
-    substitutes: ['essay advice service'],
+    word: 'unlimited revisions',
+    substitutes: [
+      'final touch',
+      'unlimited improvements',
+      'unlimited preparation',
+      'unlimited edits',
+      'unlimited changes',
+      'unlimited corrections',
+      'unlimited alternations',
+      'review improvements',
+      'Review the assignment',
+      'two-proofreader model',
+    ],
+  },
+
+  {
+    word: 'scratch',
+    substitutes: [
+      'from ground up',
+      'We’ll provide an in-depth content review designed to help you refine your narrative and showcase qualities that will add value to any institution. Your coach will make sure you cover your bases and tell a powerful story that says you’re institution ready.',
+      'Our solutions are specifically tailored to align with your tutoring needs.',
+      'Experts will help you express your ideas and find the right voice for your story. You’ll receive a polished paper and writing tips.',
+    ],
   },
 ];
 
